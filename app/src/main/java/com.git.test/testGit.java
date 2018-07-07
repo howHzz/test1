e0b65709c7e8a3825b8bd05bcffed44745ed94a9
hzz <455853936@qq.com>
2018-07-07 16:03:47 +0800

    change testGit sayHello as private

---
+++ app/src/main/java/com.git.test/testGit.java
@@ -0,0 +1,21 @@
+package com.git.test;
+
+import org.junit.Test;
+
+public class testGit {
+
+    public void sayHello(){
+
+        System.out.print("hello world");
+
+
+        System.out.print("你镇好");
+
+        System.out.print("congratulation");
+    }
+
+    @Test
+    public void test(){
+        sayHello();
+    }
+}
